f6256d9351f1cfc95859bef115acfc638d8cf337
Alef oliveira <you@example.com>
2020-04-10 01:54:26 -0300

    mobile

--- mobile/App.js
+++ mobile/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+
+import Routes from '/src/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Alef</Text>
+    <View>
+      <Routes />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#1E90FF',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
